ce1d8af373435bc69bab2a15486e5ef3262935ad
OchoaBot <91572024+OchoaBot@users.noreply.github.com>
2021-12-24 15:18:17 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -2,12 +2,14 @@ const version = (prefix, pushname) => {
     return `
 *XavyBot by Ochoa*
 
-*Actualizado:* 06 de Diciembre del 2021
-*Versión actual:* 3.0
-*Ofrecida por:* shanduy™
+*Actualizado:* 24 de Diciembre del 2021
+*Versión actual:* 4.0
+*Ofrecida por:* Ochoa™
 
 
 *INFORME*
+
+FELIS NABIDA, TEAMO
 
 Si no funciona el comando !play o !play2 revisa el blog del bot y mira la version que poses 
 Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
